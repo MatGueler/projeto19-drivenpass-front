196f42cd0d5e75ad76b1ea2925f6ed018b6ebeb3
MattGueler <mateus.gueler@gmail.com>
2022-09-10 18:49:21 -0300

    Create a credential layout

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import CredentialScreen from "../Pages/Credentials/CredentialsScreen";
 import LoginScreen from "../Pages/Login/LoginScreen";
 import MenuScreen from "../Pages/Menu/MenuScreen";
 import RegisterScreen from "../Pages/Register/RegisterScreen";
@@ -12,8 +13,8 @@ function App() {
         <Route path={"/"} element={<LoginScreen />} />
         <Route path={"/sign-up"} element={<RegisterScreen />} />
         <Route path={"/menu"} element={<MenuScreen />} />
-        {/*   <Route path={"/hashtag/:hashtag"} element={<HashTagPage />} />
-          <Route path={"/user/:id"} element={<UserPage />} /> */}
+        <Route path={"/credentials"} element={<CredentialScreen />} />
+        {/*  <Route path={"/user/:id"} element={<UserPage />} /> */}
       </Routes>
     </BrowserRouter>
     // {
--- src/Components/Pages/Credentials/CredentialsScreen.jsx
+++ src/Components/Pages/Credentials/CredentialsScreen.jsx
@@ -3,36 +3,41 @@
 // import { BASE_URL, getCookieByName } from "../../../mock/data.js";
 // import UserContext from "../../contexts/UserContext";
 import { Container } from "../../Container/Container";
-import { Icon, Main, Quantify, Record, Records } from "./MenuStyle";
-import { IoExit } from "react-icons/io5";
+import {
+  Icon,
+  Main,
+  NewCredential,
+  Quantify,
+  Record,
+  Records,
+} from "./CredentialsStyle";
 import { Header2 } from "../../Templates/Header/Header";
+import { IoExit } from "react-icons/io5";
+import { IoIosAddCircle } from "react-icons/io";
 
-export default function MenuScreen() {
+export default function CredentialScreen() {
   return (
     <Container>
       <Header2 />
       <Main>
-        <h2>Minhas senhas</h2>
+        <h2>Credenciais</h2>
         <Records>
           <Record>
             <Icon>
               <IoExit size={50} color="#0a5286" />
             </Icon>
-            <h3>Nome do registro</h3>
-            <Quantify>
-              <p>4</p>
-            </Quantify>
+            <h3>Site 1</h3>
           </Record>
           <Record>
             <Icon>
               <IoExit size={50} color="#0a5286" />
             </Icon>
-            <h3>Nome do registro</h3>
-            <Quantify>
-              <p>4</p>
-            </Quantify>
+            <h3>Site 2</h3>
           </Record>
         </Records>
+        <NewCredential>
+          <IoIosAddCircle size={60} color="#0a5286" cursor="pointer" />
+        </NewCredential>
       </Main>
     </Container>
   );
